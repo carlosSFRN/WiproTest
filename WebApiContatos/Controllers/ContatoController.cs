57ee1034cb1a4cbba669752efdcb7836485f6c23
carlosSFRN <carlos505monks@hotmail.com>
2021-05-10 18:02:03 -0300

    Inclusao controller e os metodos http

---
+++ WebApiContatos/Controllers/ContatoController.cs
@@ -0,0 +1,171 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using WebApiContatos.Models;
+using System.Data.SqlClient;
+
+namespace WebApiContatos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContatoController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ContatoController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            List<Contato> listaContato = new List<Contato>();
+
+            string connect = _configuration.GetConnectionString("AgendaDB");
+
+            using (SqlConnection con = new SqlConnection(connect))
+            {
+                SqlCommand cmd = new SqlCommand("stp_Contato_Sel", con)
+                {
+                    CommandType = CommandType.StoredProcedure,
+                    CommandTimeout = 60
+                };
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Contato usuario = new Contato
+                    {
+                        IdPessoa = Convert.ToInt32(rdr["idPessoa"]),
+                        Nome = rdr["nome"].ToString(),
+                        DataNascimento = Convert.ToDateTime(rdr["dataNascimento"].ToString()),
+                        Observacoes = rdr["observacoes"].ToString(),
+                        Telefone = rdr["telefone"].ToString(),
+                        Email = rdr["email"].ToString()
+                    };
+
+                    listaContato.Add(usuario);
+                }
+                con.Close();
+            }
+            return new JsonResult(listaContato);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult Get(int id)
+        {
+            List<Contato> listaContato = new List<Contato>();
+
+            string connect = _configuration.GetConnectionString("AgendaDB");
+
+            using (SqlConnection con = new SqlConnection(connect))
+            {
+                SqlCommand cmd = new SqlCommand("stp_Contato_Sel_Id", con)
+                {
+                    CommandType = CommandType.StoredProcedure,
+                    CommandTimeout = 60
+                };
+
+                cmd.Parameters.AddWithValue("@idPessoa", id);
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Contato usuario = new Contato
+                    {
+                        IdPessoa = Convert.ToInt32(rdr["idPessoa"]),
+                        Nome = rdr["nome"].ToString(),
+                        DataNascimento = Convert.ToDateTime(rdr["dataNascimento"].ToString()),
+                        Observacoes = rdr["observacoes"].ToString(),
+                        Telefone = rdr["telefone"].ToString(),
+                        Email = rdr["email"].ToString()
+                    };
+
+                    listaContato.Add(usuario);
+                }
+                con.Close();
+            }
+            return new JsonResult(listaContato);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Contato contato)
+        {
+            string connect = _configuration.GetConnectionString("AgendaDB");
+
+            using (SqlConnection con = new SqlConnection(connect))
+            {
+                SqlCommand cmd = new SqlCommand("stp_Contato_Ins", con)
+                {
+                    CommandType = CommandType.StoredProcedure,
+                    CommandTimeout = 60
+                };
+
+                cmd.Parameters.AddWithValue("@Nome", contato.Nome);
+                cmd.Parameters.AddWithValue("@DataNascimento", contato.DataNascimento);
+                cmd.Parameters.AddWithValue("@Observacoes", contato.Observacoes);
+                cmd.Parameters.AddWithValue("@Telefone", contato.Telefone);
+                cmd.Parameters.AddWithValue("@Email", contato.Email);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+
+        [HttpPut]
+        public void Edit(Contato contato)
+        {
+            string connect = _configuration.GetConnectionString("AgendaDB");
+
+            using (SqlConnection con = new SqlConnection(connect))
+            {
+                SqlCommand cmd = new SqlCommand("stp_Contato_Upd", con)
+                {
+                    CommandType = CommandType.StoredProcedure,
+                    CommandTimeout = 60
+                };
+
+                cmd.Parameters.AddWithValue("@idPessoa", contato.IdPessoa);
+                cmd.Parameters.AddWithValue("@Nome", contato.Nome);
+                cmd.Parameters.AddWithValue("@DataNascimento", contato.DataNascimento);
+                cmd.Parameters.AddWithValue("@Observacoes", contato.Observacoes);
+                cmd.Parameters.AddWithValue("@Telefone", contato.Telefone);
+                cmd.Parameters.AddWithValue("@Email", contato.Email);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        public void Delete(int? id)
+        {
+            string connect = _configuration.GetConnectionString("AgendaDB");
+
+            using (SqlConnection con = new SqlConnection(connect))
+            {
+                SqlCommand cmd = new SqlCommand("stp_Contato_Del", con)
+                {
+                    CommandType = CommandType.StoredProcedure,
+                    CommandTimeout = 60
+                };
+
+                cmd.Parameters.AddWithValue("@idPessoa", id);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+    }
+}
---
+++ WebApiContatos/Models/Contato.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiContatos.Models
+{
+    public class Contato
+    {
+        public int IdPessoa { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Observacoes { get; set; }
+        public string Telefone  { get; set; }
+        public string Email { get; set; }
+    }
+}
